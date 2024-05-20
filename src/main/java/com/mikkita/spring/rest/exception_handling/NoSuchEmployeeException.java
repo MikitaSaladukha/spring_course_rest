906436bd606bc8e8bf3af3a0f7ab9eb8dedeadd9
Mikita Saladukha <malkomus@gmail.com>
2024-05-20 17:35:42 +0300

    local exception handling

---
+++ src/main/java/com/mikkita/spring/rest/exception_handling/NoSuchEmployeeException.java
@@ -0,0 +1,7 @@
+package com.mikkita.spring.rest.exception_handling;
+
+public class NoSuchEmployeeException extends RuntimeException {
+    public NoSuchEmployeeException(String message) {
+        super(message);
+    }
+}
